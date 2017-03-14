80a0700b35e614c593bc2e7e2e6b4d8f6d62a96f
B_Molina <bm140314@ohio.edu>
2017-03-14 12:04:05 -0400

    Save

---
+++ Animation tutorial/New Unity Project/Assets/doorcontroller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class doorcontroller : MonoBehaviour {
+
+	Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator>
+
+	}
+
+	void OnGUI () {
+
+		if (GUILayout.Button ("Open Door")) 
+		{
+			Animator.SetInteger ("Door close", 1);
+			Animator.SetInteger ("Door open", 0);
+		}
